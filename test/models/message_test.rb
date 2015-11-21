2f138e295112e493a81f24b3ab80093448e04ade
Joshua Echeverri <josheche@gmail.com>
2015-11-21 17:47:03 -0500

    pass it

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -7,4 +7,16 @@ class MessageTest < ActiveSupport::TestCase
       assert msg.respond_to? attr
     end
   end
+  test 'should accept valid attributes' do
+    valid_attrs = {
+      name: 'josh ',
+      email: 'josheche@gmail.com',
+      subject: 'hi',
+      content: 'you good?'
+    }
+
+    msg = Message.new valid_attrs
+
+    assert msg.valid?
+  end
 end
